7c9c2b37f65ff09340785c1eb4cc4dd67137bb3f
Brae0526 <bmahnke@umich.edu>
2023-10-10 19:19:13 -0400

    Added example code for clock logic

--- main/board_controller.c
+++ main/board_controller.c
@@ -20,15 +20,16 @@ void app_main(void)
     int ones_minutes = 0;
     int tens_seconds = 0;
     int ones_seconds = 1;
-    while(hours != 0 && tens_minutes != 0 && ones_minutes !=0 && tens_seconds != 0 && ones_seconds!= 0){
+    //While loop is for clock logic
+    while(hours + tens_minutes +tens_seconds +ones_minutes + ones_seconds != 0){
         if(ones_seconds == 0){
             ones_seconds = 9;
             if(tens_seconds == 0){
-                tens_seconds = 9;
+                tens_seconds = 5;
                 if(ones_minutes == 0){
                     ones_minutes = 9;
                     if(tens_minutes == 0 && hours !=0){
-                        tens_minutes = 9;
+                        tens_minutes = 5;
                         hours -= 1;
                     }
                     else{
@@ -47,9 +48,7 @@ void app_main(void)
         else{
             ones_seconds -=1;
         }
-        vTaskDelay(pdMS_TO_TICKS(1000000000000000));
+        vTaskDelay(pdMS_TO_TICKS(1000));
         GraphicLCD_DispNHDPic(hours, tens_minutes, ones_minutes, tens_seconds, ones_seconds);
     }
-
-    GraphicLCD_DispNHDPic(1, 4,6, 3, 2);
 }
